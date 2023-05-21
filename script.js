6696667254c628fc84a782e4a2ad527387785ead
Hemanshu <himanshusrigiri@gmail.com>
2023-05-21 17:47:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+  strings: ["Web Design", "Web Developer", "Frontend Developer", "Backened Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true
+
+})
---
+++ style-switcher.js
@@ -0,0 +1,42 @@
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    console.log("Clicked");
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark");
+
+    // Add the following lines to toggle the education title color
+    const educationTitle = document.querySelector(".education .title");
+    educationTitle.classList.toggle("dark");
+});
+
+window.addEventListener("load", () => {
+    if (document.body.classList.contains("dark")) {
+        dayNight.querySelector("i").classList.add("fa-sun");
+    } else {
+        dayNight.querySelector("i").classList.add("fa-moon");
+    }
+});
